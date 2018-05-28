310a218f84e561459f3ae510bc6c2d5bd421123b
changhee <tkzkeldhs@naver.com>
2018-05-28 18:45:17 +0900

    액티비티 추가

--- waikiki/app/src/main/java/com/security/waikiki/myapplication/view/activity/InstallSplashActivity.java
+++ waikiki/app/src/main/java/com/security/waikiki/myapplication/view/activity/InstallSplashActivity.java
@@ -3,12 +3,11 @@ package com.security.waikiki.myapplication.view.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-public class EventActivity extends RootParentActivity {
+public class InstallSplashActivity extends RootParentActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
 }
--- waikiki/app/src/main/java/com/security/waikiki/myapplication/view/activity/InviteActivity.java
+++ waikiki/app/src/main/java/com/security/waikiki/myapplication/view/activity/InviteActivity.java
@@ -1,4 +1,13 @@
 package com.security.waikiki.myapplication.view.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 public class InviteActivity extends RootParentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
